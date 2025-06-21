beceb4ac286ab49ece31b0a8954706d58ec5b1ce
Liza <kalanchuk.liza@mail.ru>
2025-06-21 16:12:58 +0300

    Исправлены файлы

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Java CI with Gradle](https://github.com/ElizavetaKalanchuk/selenide1/actions/workflows/gradle.yml/badge.svg)](https://github.com/ElizavetaKalanchuk/selenide1/actions/workflows/gradle.yml)
--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,35 +1,41 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
-    @Test
-    void shouldSubmitRequest() {
+    @BeforeEach
+    void setup() {
+        Configuration.headless = true;
         open("http://localhost:9999");
+    }
 
-        // Заполнение формы
+    private String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void shouldSubmitSuccessfulRequest() {
+        String date = generateDate(3);
+
         $("[data-test-id=city] input").setValue("Москва");
-
-        // Генерация даты (текущая дата + 3 дня)
-        String deliveryDate = LocalDate.now()
-                .plusDays(3)
-                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
-        $("[data-test-id=date] input").doubleClick().sendKeys(deliveryDate);
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(date);
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
 
-        // Проверка успешной отправки
-        $("[data-test-id=notification]")
-                .shouldHave(Condition.text("Успешно!"))
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + date),
+                        Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
     }
 }
