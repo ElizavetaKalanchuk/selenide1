ee89a1198612218d76823f9c71986bf80bade812
Liza <kalanchuk.liza@mail.ru>
2025-06-29 19:28:01 +0300

    Изменены все файлы

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,67 +1,204 @@
-package ru.netology.delivery;
+package ru.netology;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.*;
-import java.time.Duration;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
 
 public class CardDeliveryTest {
-
-    @BeforeAll
-    static void setUpAll() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.browser = "chrome";
-        Configuration.headless = false;
-        Configuration.timeout = 15000;
-        Configuration.pageLoadTimeout = 15000;
-    }
-
-    @BeforeEach
-    void setUp() {
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").shouldBe(visible, Duration.ofSeconds(10));
+    private String generateDate(int days, String pattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
-    @DisplayName("Should successful plan meeting")
-    void shouldSubmitRequest() {
-        // Генерируем тестовые данные
-        var user = DataGenerator.Registration.generateUser("ru");
-        var deliveryDate = DataGenerator.generateDate(4);
+    void successfulSendingOfData() { //успешная отправка данных
+        String planingDate = generateDate(3, "dd.MM.yyyy");
+        Selenide.open("http://localhost:9999/");
 
-        // Заполняем город
-        $("[data-test-id=city] input").setValue(user.getCity().substring(0, 2));
-        $$(".menu-item").findBy(text(user.getCity())).click();
-
-        // Очищаем поле даты и заполняем
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(deliveryDate);
-
-        // Заполняем остальные поля
-        $("[data-test-id=name] input").setValue(user.getName());
-        $("[data-test-id=phone] input").setValue(user.getPhone());
-        $("[data-test-id=agreement]").click();
-
-        // Отправляем форму
-        $$("button").find(exactText("Забронировать")).click();
-
-        // Ожидаем появления уведомления (более надежный способ)
-        $(".notification").shouldBe(visible, Duration.ofSeconds(15));
-
-        // Проверяем текст в уведомлении
-        $(".notification .notification__content")
-                .shouldHave(text("Встреча успешно забронирована на " + deliveryDate))
-                .shouldBe(visible);
+        $("[data-test-id='city'] input").shouldBe(visible, enabled).setValue("Ниж");
+        $$(".menu-item").findBy(text("Нижний Новгород")).shouldBe(visible).click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, planingDate);
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $$("button").findBy(text("Забронировать")).click();
+        $("[data-test-id='notification'] .notification__title").shouldHave(text("Успешно"), Duration.ofSeconds(15)).shouldBe(visible);
+        $("[data-test-id='notification'] .notification__content").shouldHave(text("Встреча успешно забронирована на " + planingDate), Duration.ofSeconds(15)).shouldBe(visible);
     }
 
-    @AfterEach
-    void tearDown() {
-        closeWebDriver();
+    @Test
+    void theCityFieldIsNotFilledIn() { // не заполнено поле город
+        String planingDate = generateDate(3, "dd.MM.yyyy");
+        Selenide.open("http://localhost:9999/");
+
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, planingDate);
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $$("button").findBy(text("Забронировать")).click();
+        $$("[data-test-id='city'].input_invalid .input__sub").findBy(text("Поле обязательно для заполнения")).shouldBe(visible);
+    }
+
+    @Test
+    void dateFieldIsNotFilledIn() { //не заполнено поле дата
+        String planingDate = generateDate(3, "dd.MM.yyyy");
+        Selenide.open("http://localhost:9999/");
+
+        $("[data-test-id='city'] input").shouldBe(visible, enabled).setValue("Ниж");
+        $$(".menu-item").findBy(text("Нижний Новгород")).shouldBe(visible).click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $$("button").findBy(text("Забронировать")).click();
+        $$("[data-test-id='date'] .input__sub").findBy(text("Неверно введена дата")).shouldBe(visible);
+    }
+
+    @Test
+    void theNameFieldIsNotFilledIn() { // не заполнено поле имя
+        String planingDate = generateDate(3, "dd.MM.yyyy");
+        Selenide.open("http://localhost:9999/");
+
+        $("[data-test-id='city'] input").shouldBe(visible, enabled).setValue("Ниж");
+        $$(".menu-item").findBy(text("Нижний Новгород")).shouldBe(visible).click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, planingDate);
+        $("[data-test-id='name'] input").setValue("");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $$("button").findBy(text("Забронировать")).click();
+        $$("[data-test-id='name'].input_invalid .input__sub").findBy(text("Поле обязательно для заполнения")).shouldBe(visible);
+    }
+
+    @Test
+    void thePhoneFieldIsNotFilledIn() { // не заполнено поле телефон
+        String planingDate = generateDate(3, "dd.MM.yyyy");
+        Selenide.open("http://localhost:9999/");
+
+        $("[data-test-id='city'] input").shouldBe(visible, enabled).setValue("Ниж");
+        $$(".menu-item").findBy(text("Нижний Новгород")).shouldBe(visible).click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, planingDate);
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("");
+        $("[data-test-id='agreement']").click();
+        $$("button").findBy(text("Забронировать")).click();
+        $$("[data-test-id='phone'].input_invalid .input__sub").findBy(text("Поле обязательно для заполнения")).shouldBe(visible);
+    }
+
+    @Test
+    void theCheckboxIsNotMarked() { // не проставлен чекбокс
+        String planingDate = generateDate(3, "dd.MM.yyyy");
+        Selenide.open("http://localhost:9999/");
+
+        $("[data-test-id='city'] input").shouldBe(visible, enabled).setValue("Ниж");
+        $$(".menu-item").findBy(text("Нижний Новгород")).shouldBe(visible).click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, planingDate);
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $$("button").findBy(text("Забронировать")).click();
+        $$("[data-test-id='agreement'].input_invalid .checkbox__text").findBy(text("Я согласен на обработку моих персональных данных")).shouldBe(visible);
+    }
+
+    @Test
+    void invalidCity() { // не валидный город
+        String planingDate = generateDate(3, "dd.MM.yyyy");
+        Selenide.open("http://localhost:9999/");
+
+        $("[data-test-id='city'] input").shouldBe(visible, enabled).setValue("Малага");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, planingDate);
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $$("button").findBy(text("Забронировать")).click();
+        $$("[data-test-id='city'].input_invalid .input__sub").findBy(text("Доставка в выбранный город недоступна")).shouldBe(visible);
+    }
+
+    @Test
+    void invalidDate() { // не верная дата 10.66.1111"
+        Selenide.open("http://localhost:9999/");
+
+        $("[data-test-id='city'] input").shouldBe(visible, enabled).setValue("Ниж");
+        $$(".menu-item").findBy(text("Нижний Новгород")).shouldBe(visible).click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue("10.66.1111");
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $$("button").findBy(text("Забронировать")).click();
+        $$("[data-test-id='date'] .input__sub").findBy(text("Неверно введена дата")).shouldBe(visible);
+    }
+
+    @Test
+    void repeatedInvalidDate() { // не верная дата 10.02.2025
+        Selenide.open("http://localhost:9999/");
+
+        $("[data-test-id='city'] input").shouldBe(visible, enabled).setValue("Ниж");
+        $$(".menu-item").findBy(text("Нижний Новгород")).shouldBe(visible).click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue("10.02.2025");
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $$("button").findBy(text("Забронировать")).click();
+        $("[data-test-id='date'] .input__sub").shouldHave(text("Заказ на выбранную дату невозможен")).shouldBe(visible);
+    }
+
+    @Test
+    void invalidName() { // не валидная фамилия
+        String planingDate = generateDate(3, "dd.MM.yyyy");
+        Selenide.open("http://localhost:9999/");
+
+        $("[data-test-id='city'] input").shouldBe(visible, enabled).setValue("Ниж");
+        $$(".menu-item").findBy(text("Нижний Новгород")).shouldBe(visible).click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, planingDate);
+        $("[data-test-id='name'] input").setValue("Ivano Ivan Ivanovich");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $$("button").findBy(text("Забронировать")).click();
+        $$("[data-test-id='name'].input_invalid .input__sub").findBy(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.")).shouldBe(visible);
+    }
+
+    @Test
+    void invalidPhone() { // не валидный телефон
+        String planingDate = generateDate(3, "dd.MM.yyyy");
+        Selenide.open("http://localhost:9999/");
+
+        $("[data-test-id='city'] input").shouldBe(visible, enabled).setValue("Ниж");
+        $$(".menu-item").findBy(text("Нижний Новгород")).shouldBe(visible).click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, planingDate);
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("+7999999999999");
+        $("[data-test-id='agreement']").click();
+        $$("button").findBy(text("Забронировать")).click();
+        $$("[data-test-id='phone'].input_invalid .input__sub").findBy(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")).shouldBe(visible);
+    }
+
+
+    @Test
+    void dateSelection() { // выбор даты
+        String planingDate = generateDate(3, "dd.MM.yyyy");
+        Selenide.open("http://localhost:9999/");
+
+        $("[data-test-id='city'] input").shouldBe(visible, enabled).setValue("Ниж");
+        $$(".menu-item").findBy(text("Нижний Новгород")).shouldBe(visible).click();
+
+        $("button").click();
+        $("[data-step='1']").click();
+        $("[data-step='12']").click();
+        $$(".calendar__layout").findBy(text("10")).click();
+
+
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $$("button").findBy(text("Забронировать")).click();
+        $("[data-test-id='notification'] .notification__title").shouldHave(text("Успешно"), Duration.ofSeconds(15)).shouldBe(visible);
+        $("[data-test-id='notification'] .notification__content").shouldHave(text("Встреча успешно забронирована на"), Duration.ofSeconds(15)).shouldBe(visible);
     }
 }
